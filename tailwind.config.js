d5382c724b33029326bbca6ea24cc28642d9d52a
Akshay Pujari <akshaypujari013@gmail.com>
2025-06-27 01:24:03 +0000

    try tailwind generated css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./_includes/**/*.{html,js,md}",
+    "./_layouts/**/*.{html,js,md}",
+    "./_posts/**/*.{html,js,md}",
+    "./*.{html,md}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
